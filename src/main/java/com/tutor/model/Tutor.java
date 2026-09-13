b03ce4a8d2c56c381e93bde03330f15c69a633a3
Suneel-kumar204 <sunnelyadav1122@gmail.com>
2026-09-13 12:26:21 +0530

    Update Tutor.java

--- src/main/java/com/tutor/model/Tutor.java
+++ src/main/java/com/tutor/model/Tutor.java
@@ -14,32 +14,39 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="Tutor_Project")
+@Table(name = "Tutor_Project")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Tutor {
-	@Id
-	@SequenceGenerator(allocationSize = 1, initialValue = 10001,name = "tutor_seq_data" ,sequenceName = "tutor_seq_data")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator="tutor_seq_data")
-	@Column(name= "tutor_id")
-	private long tutor_id;
-	
-	@Column(name="Name",length = 25,nullable= false)
-	private String name;
-	
-	@Column(name="phoneNumber",columnDefinition = "number(10)",nullable= false)
-	private long phonenumber;
-	
-	@Column(name="email",length = 35,unique = true,nullable= false)
-	private String email;
-	
-	@Column(name="City",length = 20,nullable= false)
-	private String city;
-	
-	@Column(name="Course",length = 25,nullable= false)
-	private String course;
 
+    @Id
+    @SequenceGenerator(
+        name = "tutor_seq_data",
+        sequenceName = "tutor_seq_data",
+        allocationSize = 1,
+        initialValue = 10001
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "tutor_seq_data"
+    )
+    @Column(name = "tutor_id")
+    private long tutor_id;
+
+    @Column(name = "Name", length = 25, nullable = false)
+    private String name;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private long phonenumber;
+
+    @Column(name = "email", length = 35, unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "City", length = 20, nullable = false)
+    private String city;
+
+    @Column(name = "Course", length = 25, nullable = false)
+    private String course;
 }
